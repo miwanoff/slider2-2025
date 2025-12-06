9cec2bd0eaa8c756cc8680f9e318604672d7e0e0
miwanoff <marina.iva.noffa@gmail.com>
2025-12-06 13:37:25 +0200

    Add showImage

---
+++ script.js
@@ -0,0 +1,47 @@
+const images = [
+  "images/landscape-photography-tips-yosemite-valley-feature.webp",
+  "images/lanscape.jfif",
+  "images/UK_advice-gardening-growing-poppies_header.jpg",
+];
+
+window.onload = function () {
+  const sliderElement = document.getElementById("slider");
+  const nextButton = document.getElementById("next");
+  const goButton = document.getElementById("go");
+  const stopButton = document.getElementById("stop");
+
+  let mins = document.getElementsByClassName("mini");
+  for (let i = 0; i < mins.length; i++) {
+    mins[i].addEventListener("click", showImage);
+  }
+
+  nextButton.addEventListener("click", next);
+  goButton.addEventListener("click", goAuto);
+  stopButton.addEventListener("click", stop);
+};
+
+let timer;
+
+let i = 0;
+
+function next() {
+  i++;
+  if (i > images.length - 1) {
+    i = 0;
+  }
+  sliderElement.src = images[i];
+}
+
+function goAuto() {
+  stop();
+  timer = setInterval(next, 1000);
+}
+
+function stop() {
+  clearInterval(timer);
+}
+
+function showImage(event) {
+  let image = event.target;
+  //let name = image.src;
+}
